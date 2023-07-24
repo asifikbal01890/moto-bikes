7f11847639230f6e3632ab7e6bb96c99ed286de8
asifilbal01890 <asifikbal01890@gmail.com>
2023-07-24 23:29:28 +0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import NavBar from '@/Components/Shared/NavBar/NavBar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Footer from '@/Components/Shared/Footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar></NavBar>
+        {children}
+        <Footer></Footer>
+      </body>
     </html>
   )
 }
