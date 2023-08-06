68545744f5225812900cf2854e26253a15c2a320
asifilbal01890 <asifikbal01890@gmail.com>
2023-08-06 15:11:33 +0600

    test003

--- src/Components/Pages/BikesCollection/BikesCollection.tsx
+++ src/Components/Pages/BikesCollection/BikesCollection.tsx
@@ -1,4 +1,4 @@
-// 'use client'
+'use client'
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "@/app/hooks";
 import BikesFilter from "./BikesFilter/BikesFilter";
